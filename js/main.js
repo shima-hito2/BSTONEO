c7a740c345902dcbba79ee19f014199d42105ed4
puruyahha1ha <puruyahha1ha@gmail.com>
2024-09-16 23:59:30 +0900

    サードパーティーのダウンロード

--- js/main.js
+++ js/main.js
@@ -257,17 +257,17 @@ $(function () {
 //===============================================================
 // スライドショー
 //===============================================================
-$(function () {
-    var slides = $('#mainimg .slide');
-    var slideCount = slides.length;
-    var currentIndex = 0;
+// $(function () {
+//     var slides = $('#mainimg .slide');
+//     var slideCount = slides.length;
+//     var currentIndex = 0;
 
-    slides.eq(currentIndex).css('opacity', 1).addClass('active');
+//     slides.eq(currentIndex).css('opacity', 1).addClass('active');
 
-    setInterval(function () {
-        var nextIndex = (currentIndex + 1) % slideCount;
-        slides.eq(currentIndex).css('opacity', 0).removeClass('active');
-        slides.eq(nextIndex).css('opacity', 1).addClass('active');
-        currentIndex = nextIndex;
-    }, 4000); // 4秒ごとにスライドを切り替える
-});
+//     setInterval(function () {
+//         var nextIndex = (currentIndex + 1) % slideCount;
+//         slides.eq(currentIndex).css('opacity', 0).removeClass('active');
+//         slides.eq(nextIndex).css('opacity', 1).addClass('active');
+//         currentIndex = nextIndex;
+//     }, 4000); // 4秒ごとにスライドを切り替える
+// });
